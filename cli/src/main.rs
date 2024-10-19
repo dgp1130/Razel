79ce416d7b64c5522400660092feaca3b430222d
Doug Parker <dgp1130@users.noreply.github.com>
2024-10-19 16:22:54 -0700

    Parse CLI arguments with `clap`.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "Razel", version)]
+struct Args {
+  #[command(subcommand)]
+  command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+  #[command(about = "Build some targets.")]
+  Build {
+    targets: Vec<String>,
+  },
+}
+
 fn main() {
-    println!("Hello, world!");
+  let args = Args::parse();
+
+  match &args.command {
+    Command::Build { targets } => {
+      println!("Building {}", targets.join(" "));
+    }
+  }
 }
